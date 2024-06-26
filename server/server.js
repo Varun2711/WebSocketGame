d908f1ebc3613b4598c0ce10c549196f94e71aed
Varun Patel <rvjpatel99@gmail.com>
2024-06-25 22:20:42 -0500

    server setup completed

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 3000;
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
