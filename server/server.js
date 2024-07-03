4833ee67018223689a4e6b85915b4659958dc1bb
Varun Patel <rvjpatel99@gmail.com>
2024-07-03 14:52:52 -0700

    fixed error

--- server/server.js
+++ server/server.js
@@ -9,3 +9,27 @@ const port = process.env.PORT || 3000;
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+server.listen(port, () => {
+    console.log(`Server is up on port${port}`);
+});
+
+io.on('connection', (socket) => {
+    console.log('A user just connected.');
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected');
+    });
+
+    socket.on('startGame', () => {
+        io.emit('startGame');
+    });
+
+    socket.on('crazyIsClicked', (data) => {
+        io.emit('crazyIsClicked', data);
+    })
+});
+
+
+
